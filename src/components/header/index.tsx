46d67cb135e99ee408d28ceef1e2c76198aa6eae
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-29 15:03:54 -0300

    feat: criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { LiaGamepadSolid } from "react-icons/lia";
+
+import logoImg from "public/logo.svg";
+
 export function Header() {
   return (
     <header className="w-full h-28 bg-slate-100 text-black px-2">
-      <h1>Header Teste</h1>
+      <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              className="w-full"
+              src={logoImg}
+              alt="Logo site DailyGames"
+              quality={100}
+              priority
+            />
+          </Link>
+
+          <Link href="/">Games</Link>
+
+          <Link href="/profile">Perfil</Link>
+        </nav>
+
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <LiaGamepadSolid size={34} color="#475569" />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
