e5bab032a3939f53ed97dd375572d0f581a3863b
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-29 18:04:34 -0300

    feat: implementando funcinalidade de buscar sugestao de jogo via api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 import { Container } from "@/components/container";
 import { GameProps } from "@/utils/types/game";
 
+import { BsArrowRightSquare } from "react-icons/bs";
+
 async function getDailyGame() {
   try {
     const res = await fetch(
-      `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 320 } }
     );
 
     return res.json();
@@ -19,8 +22,6 @@ async function getDailyGame() {
 export default async function Home() {
   const dailyGame: GameProps = await getDailyGame();
 
-  console.log(dailyGame);
-
   return (
     <main className="w-full">
       <Container>
@@ -29,14 +30,23 @@ export default async function Home() {
         </h1>
         <Link href={`/game/${dailyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image
-              src={dailyGame.image_url}
-              alt={dailyGame.title}
-              priority
-              quality={100}
-              width={100}
-              height={100}
-            />
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">
+                  {dailyGame.title}
+                </p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+              <Image
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                src={dailyGame.image_url}
+                alt={dailyGame.title}
+                priority
+                quality={100}
+                fill={true}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+              />
+            </div>
           </section>
         </Link>
       </Container>
